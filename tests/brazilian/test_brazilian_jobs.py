19176696aaf09172cb42e340a5d2508fbeeebd5f
Braien Machado de Paula <braienmp@outlook.com>
2022-07-04 17:28:49 -0300

    test: brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    list = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for job in list:
+        for header, data in list[0].items():
+            assert header in ['title', 'salary', 'type']
