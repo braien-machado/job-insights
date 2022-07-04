fa8e8e446413dfc9da180613c34ce718de84d2f7
Braien Machado de Paula <braienmp@outlook.com>
2022-07-04 17:30:28 -0300

    style: use black

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,7 +2,7 @@ from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    list = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    list = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
     for job in list:
         for header, data in list[0].items():
-            assert header in ['title', 'salary', 'type']
+            assert header in ["title", "salary", "type"]
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,7 +2,7 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    counter_lower_case = count_ocurrences('src/jobs.csv', 'Python')
-    counter_upper_case = count_ocurrences('src/jobs.csv', 'PYTHON')
+    counter_lower_case = count_ocurrences("src/jobs.csv", "Python")
+    counter_upper_case = count_ocurrences("src/jobs.csv", "PYTHON")
 
     assert counter_lower_case == counter_upper_case == 1639
