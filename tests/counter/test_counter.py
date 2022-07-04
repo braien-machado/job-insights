4de7e96ca6e58396b162663f8ef8d88c0b750fe9
Braien Machado de Paula <braienmp@outlook.com>
2022-07-04 17:17:18 -0300

    test: count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    counter_lower_case = count_ocurrences('src/jobs.csv', 'Python')
+    counter_upper_case = count_ocurrences('src/jobs.csv', 'PYTHON')
+
+    assert counter_lower_case == counter_upper_case == 1639
